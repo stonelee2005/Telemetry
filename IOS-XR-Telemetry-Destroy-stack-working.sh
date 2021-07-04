4c078697f5ad3d7e9d63468931100a5544a5ce3d
stonelee2005 <comebackhome@163.com>
2021-07-04 15:57:44 +0800

    XR_TCS Fix bug version

---
+++ IOS-XR-Telemetry-Destroy-stack-working.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#######################################################
+######### Telemetry Collection Destroy script #########
+#######################################################
+
+#######################################################
+######## This removes InfluxDB from your server #######
+#######################################################
+echo -e "\e[1;46m Removing InfluxDB \e[0m";
+PID=`pgrep influxd`
+kill -9 $PID
+dpkg --purge influxdb
+echo -e "\e[1;45m Done! \e[0m";
+#######################################################
+
+
+#######################################################
+####### This removes Telegraf from your server ########
+#######################################################
+echo -e "\e[1;46m Removing Telegraf \e[0m";
+PID=`pgrep telegraf`
+kill -9 $PID
+dpkg --purge telegraf
+echo -e "\e[1;45m Done! \e[0m";
+#######################################################
+
+
+#######################################################
+####### This removes Kapacitor from your server #######
+#######################################################
+echo -e "\e[1;46m Removing Kapacitor \e[0m";
+dpkg --purge kapacitor
+PID=`pgrep kapacitor`
+kill -9 $PID
+rm -r /var/lib/kapacitor
+rm -r /var/log/kapacitor
+PID=`ps -ef | grep KAPACITOR-HELPER-CPU | \
+grep -v "grep" | awk '{print $2}'`
+kill -9 $PID
+echo -e "\e[1;45m Done! \e[0m";
+#######################################################
+
+
+#######################################################
+###### This removes Prometheus from your server #######
+#######################################################
+echo -e "\e[1;46m Removing Prometheus \e[0m";
+PID=`pgrep prometheus`
+kill -9 $PID
+echo -e "\e[1;45m Done! \e[0m";
+#######################################################
+
+
+#######################################################
+######## This removes Grafana from your server ########
+#######################################################
+echo -e "\e[1;46m Removing Grafana \e[0m";
+PID=`pgrep grafana`
+kill -9 $PID
+dpkg --purge grafana
+rm -r /etc/grafana
+rm -r /var/lib/grafana
+echo -e "\e[1;45m Done! \e[0m";
+#######################################################
+
+
+#######################################################
+######## This removes Pipeline from your server #######
+#######################################################
+echo -e "\e[1;46m Removing Pipeline \e[0m";
+PID=`pgrep pipeline`
+kill -9 $PID
+pkill screen 2>/dev/null
+echo -e "\e[1;45m Every component was removed! \e[0m";
+
+#######################################################
+########         End of the script              #######
+#######################################################
